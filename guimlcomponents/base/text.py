80e2442d3571a8242cb4e7a47ee3f7a4d561f37a
Stephan Gocht <gocht@mailbox.org>
2023-10-06 20:41:20 +0200

    Refactoring.

--- guimlcomponents/base/text.py
+++ guimlcomponents/base/text.py
@@ -57,100 +57,8 @@ def get_extent(text, font_size):
     font = cairo.ScaledFont(fontFace, fontMatrix, user2device, options)
     return TextExtents(*font.text_extents(text))
 
-# @component(
-#     name = "input",
-#     template = """<template><text py_text="self.escaped_text"></text></template>"""
-# )
-class Input(Div):
-    @dataclass
-    class Properties(Div.Properties):
-        text: str = None
-        on_text: Optional[Callable] = None
-
-    @dataclass
-    class Dependencies(Div.Dependencies):
-        text_control: TextControl
-
-    @property
-    def escaped_text(self):
-        return escape(self.text)
-
-    @property
-    def text(self):
-        if self.properties.text is not None:
-            return self.properties.text
-        else:
-            return self._text
-
-    @text.setter
-    def text(self, value):
-        if self.properties.text is not None:
-            self.properties.text = value
-        else:
-            self._text = value
-
-    def on_init(self):
-        super().on_init()
-        self._text = ""
-        self.cursor_position = 0
-        text_control = self.dependencies.text_control
-        subscription = text_control.on_text.subscribe(self.on_text)
-        self._on_text_subscription = subscription
-        subscription = text_control.on_text_motion.subscribe(self.on_text_motion)
-        self._on_text_motion_subscription = subscription
-
-    def on_text(self, text):
-        if text:
-            self.text = self.text[:self.cursor_position] + text + self.text[self.cursor_position:]
-            self.cursor_position += len(text)
-
-            if self.properties.on_text is not None:
-                self.properties.on_text(self.text)
-
-    def on_draw(self, context):
-        super().on_draw(context)
-
-        with context:
-            font_size = 14
-            cursor_x_position = get_extent(self.text[:self.cursor_position], font_size).x_advance
-
-            content = self.content_position
-            cursor_width = 2
-
-            context.rectangle(content.left + cursor_x_position, content.top + 2, cursor_width, font_size)
-            pat = cairo.SolidPattern(0, 0, 0, 1)
-            context.set_source(pat)
-            context.fill()
-
-    def on_text_motion(self, motion):
-        if motion == pyglet_key.MOTION_BACKSPACE:
-            if self.cursor_position > 0:
-                self.text = self.text[:self.cursor_position - 1] + self.text[self.cursor_position:]
-                self.cursor_position -= 1
-                self.cursor_position = max(self.cursor_position, 0)
-        elif motion == pyglet_key.MOTION_DELETE:
-            self.text = self.text[:self.cursor_position] + self.text[self.cursor_position + 1:]
-
-        elif motion == pyglet_key.MOTION_LEFT:
-            self.cursor_position -= 1
-            self.cursor_position = max(self.cursor_position, 0)
-        elif motion == pyglet_key.MOTION_RIGHT:
-            self.cursor_position += 1
-            self.cursor_position = min(self.cursor_position, len(self.text))
-        elif motion == pyglet_key.MOTION_BEGINNING_OF_LINE:
-            self.cursor_position = 0
-        elif motion == pyglet_key.MOTION_END_OF_LINE:
-            self.cursor_position = len(self.text)
-
-
-
-    def on_destroy(self):
-        self._on_text_subscription.cancel()
-        self._on_text_motion_subscription.cancel()
-
-
 @component("text")
-class Text(DrawableComponent):
+class Text(DrawableComponent, Subscriber):
     @dataclass
     class Properties(DrawableComponent.Properties):
         text: str = ''
@@ -160,8 +68,26 @@ class Text(DrawableComponent):
     class Dependencies(DrawableComponent.Dependencies):
         pango: PangoContext
 
+    def on_init(self):
+        super().on_init()
+
+        self.selection_start = None
+        self.selection_end = None
+
+    def on_destroy(self):
+        self.cancel_subscriptions()
+
     def get_display_text(self):
         return self.properties.text
+
+    def add_selection(self, text):
+        if self.selection_start is not None:
+            start = min(self.selection_start, self.selection_end)
+            stop = max(self.selection_start, self.selection_end)
+
+            return f'{text[:start]}<span fgcolor="white" bgcolor="blue">{text[start:stop]}</span>{text[stop:]}'
+        else:
+            return text
 
     def get_layout(self):
         layout = pango.Layout(self.dependencies.pango.context)
@@ -201,8 +127,9 @@ class Text(DrawableComponent):
         # 4.3) Layout::get_cursor_pos
         # 4.4) Layout::get_caret_pos
 
+#     template = """<template><text py_text="self.escaped_text"></text></template>"""
 @component(name = "input")
-class RawInput(Text, Subscriber):
+class RawInput(Text):
     @dataclass
     class Properties(Text.Properties):
         text: str = None
@@ -218,28 +145,18 @@ class RawInput(Text, Subscriber):
         self._text = ""
         self.cursor_position = 0
 
-        self.selection_start = 1
-        self.selection_end = 4
-
         text_control = self.dependencies.text_control
         self.subscribe('on_text', text_control)
         self.subscribe('on_text_motion', text_control)
         self.subscribe('on_text_motion_select', text_control)
 
     def on_destroy(self):
-        self.cancel_subscriptions()
+        super().on_destroy()
 
     def get_display_text(self):
         text = escape(self.text)
-
-        if self.selection_start is not None:
-            start = min(self.selection_start, self.selection_end)
-            stop = max(self.selection_start, self.selection_end)
-
-            return f'{text[:start]}<span fgcolor="white" bgcolor="blue">{text[start:stop]}</span>{text[stop:]}'
-
-        else:
-            return text
+        text = self.add_selection(text)
+        return text
 
     @property
     def text(self):
